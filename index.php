51380245e6592e11768695eb06214333150a9614
monica-lopez <monicalopez.bv@gmail.com>
2026-06-12 10:26:35 -0500

    Añade el archivo index: enrutador principal

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+// ======================================================================================
+// ARCHIVO: ENRUTADOR INICIAL
+// Actúa como el punto de entrada principal del proyecto.
+// Redirige el flujo hacia la pantalla de inicio de sesión.
+// ======================================================================================
+
+header("Location: pages/login.php");
+exit();
+?>
